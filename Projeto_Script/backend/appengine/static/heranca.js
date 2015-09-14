1f9a3eaa35f53fd24da9199631b438338094efe0
JeffreiToledo <jef_mor@hotmail.com>
2015-09-13 21:28:39 -0300

    Removendo imagens

---
+++ Projeto_Script/backend/appengine/static/heranca.js
@@ -0,0 +1,39 @@
+function construtorObservador() {
+
+  var lista = [];
+  var evento = {contador:0};
+
+  var objeto = {
+
+    contar: function () {
+      lista.forEach(function (valor) {
+        valor(evento);
+        evento.contador ++;
+      });
+    }
+  };
+
+  function adicionarOuvinte(funcao) {
+    lista.push(funcao);
+
+  }
+
+  objeto.adicionarOuvinte = adicionarOuvinte;
+
+  return objeto;
+}
+
+function observador(evento) {
+  console.log('Contando...' + evento.contador);
+}
+
+
+
+var contadorObservador = construtorObservador();
+console.log(contadorObservador);
+
+contadorObservador.adicionarOuvinte(observador);
+
+
+contadorObservador.contar();
+contadorObservador.contar();
